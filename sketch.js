de9375c87a00cc50abb6f97771713cbe8340c132
YsaWV <YsaWV@users.noreply.github.com>
2022-11-22 12:12:02 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,12 @@ let ampHistory = [];
 
 // audio file is loaded before anything else starts
 function preload() {
-  song = loadSound('https://github.com/EBPL-Tech-Training/Dancing_Unicorns/blob/main/pinkunicorns.mp3?raw=true');
+  const music = new Audio('pinkunicorns.mp3');
+music.play();
+music.loop =true;
+music.playbackRate = 2;
+music.pause();qqazszdgfbgtyj
+  //song = loadSound('https://github.com/EBPL-Tech-Training/Dancing_Unicorns/blob/main/pinkunicorns.mp3?raw=true');
 }
 
 
@@ -29,9 +34,9 @@ function setup() {
   slider.position(10, 200);
 // Song Visualization Set up
 // Play the song file
-  song.play();
+  //song.play();
 //Start an amplitude object (Song Volume)
- amp = new p5.Amplitude();
+ //amp = new p5.Amplitude();
   
    
    
@@ -44,27 +49,27 @@ function draw() {
    background(151,191,221);
 
 //Control the Volume of the song with the slider
-  song.setVolume(slider.value());
+  //song.setVolume(slider.value());
 
 // Collect the Volume ( amplitude data) between 0 and 1. 
-  let ampValue = amp.getLevel();
+  //let ampValue = amp.getLevel();
 // Add amplitude data into an Array
-  ampHistory.push(ampValue);
+  //ampHistory.push(ampValue);
 
   stroke(255);
   noFill();
 //Start connecting all the points dwarn by Vertex.
   beginShape();
 /*Go thorough the Array and plot the points of the array. Draw the line graph*/
-  for (let i =0;i< ampHistory.length ;i++){
-    vertex(i,ampHistory[i]*500);
+ // for (let i =0;i< ampHistory.length ;i++){
+ //   vertex(i,ampHistory[i]*500);
   }
   endShape();
 //Stop connecting the dots
  
 //Keep the points moving to the left.
- if (ampHistory.length > width){
-    ampHistory.splice(0,1);
+// if (ampHistory.length > width){
+  //  ampHistory.splice(0,1);
   }
   
 // Call functions
@@ -79,32 +84,32 @@ function draw() {
 // function move(), makes the rectagles move vertically (y axis)
 
 // y depends on the values of the ampHistory array.
-function move(){
-  for (let i =0;i< ampHistory.length ;i++){
-    y = ampHistory[i]*500;
-  }
+// function move(){
+//   for (let i =0;i< ampHistory.length ;i++){
+//     y = ampHistory[i]*500;
+//   }
    
-  x= x + random(-1, 2);
-  y = y + random(-1, 2);
+//   x= x + random(-1, 2);
+//   y = y + random(-1, 2);
    
-}
+// }
 
 /*function drawRect() the rectangles are drawn at different x and y positions until they are across the width of the canvas: i < width
 x position is increased by 80 pixels each time the for loop runs. */ 
 
-function drawRect() {
-  stroke(255);
-  fill(200, 120);
-  x=0;
-// y position will be drawn based on the ampHistory Array, the volume *500. 
+// function drawRect() {
+//   stroke(255);
+//   fill(200, 120);
+//   x=0;
+// // y position will be drawn based on the ampHistory Array, the volume *500. 
 
- for (let i=0; i<width ; i++){
-  rect(x, y, r, r);
-   // second rectangle has to be different from the first rectangle, 
-//we add 25 to the x and subract 30 pixels to the y. 
-  rect(x+25, y-30, r,r);
+//  for (let i=0; i<width ; i++){
+//   rect(x, y, r, r);
+//    // second rectangle has to be different from the first rectangle, 
+// //we add 25 to the x and subract 30 pixels to the y. 
+//   rect(x+25, y-30, r,r);
    
-  x= x+80;
+//   x= x+80;
   
  }
 }
